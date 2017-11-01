9d09adb19398a512fba94d27ac792f30597caac9
jhorgan <jhorgan@lanner.com>
2017-11-01 13:20:44 +0000

    Update the script database command file to ouptut to file or screen

--- Src/FPSim.Database/pgCreateDbScript.cmd
+++ Src/FPSim.Database/pgCreateDbScript.cmd
@@ -1,1 +1,6 @@
-pg_dump --schema-only --no-owner -d fpsim -h localhost -p 5432 -U postgres -c > %1
+echo off
+if "%1"=="" (
+    pg_dump --schema-only --no-owner -d fpsim -h localhost -p 5432 -U postgres -c
+) else (
+    pg_dump --schema-only --no-owner -d fpsim -h localhost -p 5432 -U postgres -c > %1
+)
